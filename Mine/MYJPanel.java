5cefa1b971f240e5c617d33b31657aa5db4bf0f6
rwbj <34895850+rwbj@users.noreply.github.com>
2018-02-01 23:33:01 -0500

    the math is good!
    The printing of the line is not

--- Mine/MYJPanel.java
+++ Mine/MYJPanel.java
@@ -36,6 +36,8 @@ public class MYJPanel extends JPanel{
 
     Graphics2D g2d;
 	BufferedImage img=null;
+	int x1,x2,y1,y2;
+	
 
 
 	public MYJPanel() {
@@ -43,12 +45,17 @@ public class MYJPanel extends JPanel{
 		super();
 	}
 
-
+	public void setLnPts(int n1, int n2, int n3, int n4){
+		x1 = n1;
+		y1 = n2;
+		x2 = n3;
+		y2 = n4;
+	}
 
 
 	public synchronized void paintComponent(Graphics g) {
 
-		int i,j,w,h;
+		int i,j,w,h,x,y,xMax;
 
 		g2d = (Graphics2D) g;
 		g2d.drawImage(img, null, 0, 0); 
@@ -56,8 +63,26 @@ public class MYJPanel extends JPanel{
 		w = img.getWidth();
 		h = img.getHeight();
 
-		for (i=0; i<w; i++)
-			for (j=0; j<h; j++)
+		for (i=0; i<w; i++) //left to right
+			for (j=0; j<h; j++) //top to bottom
 				System.out.printf("pixel %d,%d = %x\n", i,j,img.getRGB(i,j));
+
+		
+
+		double m = (double)(y1-y2)/(double)(x1-x2);
+		if (x1<x2){
+			x=x1;
+			xMax=x2;
+		}
+		else{
+			x=x2;
+			xMax=x1;
+		}
+
+		for (;x<xMax;x++){
+			y = (int)(m*(x-x1))+y1;
+			img.setRGB(x,y,41704);
+			System.out.printf("line %d,%d = %x\n", x,y,img.getRGB(x,y));
+		}
     }
 }
